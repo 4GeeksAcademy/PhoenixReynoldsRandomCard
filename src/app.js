aff40a1fc00e49bcbeaa19076bf7f27a300780ef
Phoenix Reynolds <phoenix.b.reynolds@gmail.com>
2025-06-04 00:37:35 +0000

    Penultimate

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"]
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  
+  let cardSuit = document.querySelector('.card-top')
+  let cardButtSuit = document.querySelector('.card-butt')
+  let cardBellyNumber = document.querySelector('.card-belly')
+  const suitSelector = suits[Math.floor(Math.random() * suits.length)];
+  const numberSelector = numbers[Math.floor(Math.random() * suits.length)];
+  
+  cardSuit.innerHTML = suitSelector
+  cardButtSuit.innerHTML = suitSelector
+  cardBellyNumber.innerHTML = numberSelector
+
+  if (suitSelector==="♦" || suitSelector==="♥"){
+    cardBellyNumber.style.color = 'red';
+    cardSuit.style.color = 'red';
+    cardButtSuit.style.color = 'red';
+  }
+  console.log("funni screem");
 };
